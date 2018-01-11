b0be7b4df8fa3c318aed071da45e705b3135740d
Nitesh80 <35337336+Nitesh80@users.noreply.github.com>
2018-01-11 23:32:02 +0530

    Commit-1

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i,z[5];
+  for(i=0;i<5;i++)
+  {
+  printf("Enter the value of elements %d =",i);
+  scanf("%d",&z[i]);
+  }
+  printf("\n");
+  for(i=0;i<5;i++)
+  {
+    printf("%d\n",z[i]);
+  }
+  return 0;
+}
